629467c3813e159be389348e22441e280d1b8dcd
Renier Ricardo Figueredo <aprezcuba24@gmail.com>
2025-05-13 14:14:24 -0400

    remove write-file

--- tools/index.ts
+++ tools/index.ts
@@ -1,10 +1,8 @@
-import { writeFileTool } from "./write-file";
 import { helloWorldTool } from "./hello-world";
 import { ToolConfig } from "../types";
 import { createModuleTool } from "./create-module";
 
 export default [
-  // writeFileTool,
   helloWorldTool,
   createModuleTool,
 ] as ToolConfig<any>[];
